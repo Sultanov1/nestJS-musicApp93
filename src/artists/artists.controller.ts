d7a0a3e9d9cbd7e48a02df652bea445f3b3e52b7
Oybek Sultanov <oibekzzz09@gmail.com>
2024-10-23 16:05:25 +0600

    Реализация удаления артиста в контроллере.

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Get, NotFoundException, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { Artist, ArtistDocument } from '../schemas/artist.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -8,17 +18,17 @@ import { FileInterceptor } from '@nestjs/platform-express';
 @Controller('artists')
 export class ArtistsController {
   constructor(
-    @InjectModel(Artist.name) private artistModel: Model<ArtistDocument>
+    @InjectModel(Artist.name) private artistModel: Model<ArtistDocument>,
   ) {}
 
   @Get()
   async getArtists() {
-     return this.artistModel.find();
+    return this.artistModel.find();
   }
 
   @Get(':id')
   async getOneArtist(@Param('id') id: string) {
-    const artist = await this.artistModel.findOne({_id: id});
+    const artist = await this.artistModel.findOne({ _id: id });
 
     if (!artist) {
       throw new NotFoundException(`Product with id ${id} not found`);
@@ -32,11 +42,26 @@ export class ArtistsController {
   async createArtist(
     @Body() artistDto: CreateArtistDto,
     @UploadedFile() file: Express.Multer.File,
-    ) {
+  ) {
     return await this.artistModel.create({
       name: artistDto.name,
       info: artistDto.info,
       image: file ? 'images/' + file.filename : null,
-    })
+    });
+  }
+
+  @Delete(':id')
+  async deleteArtist(@Param('id') id: string) {
+    try {
+      const artist = await this.artistModel.deleteOne({ _id: id });
+
+      if (artist.deletedCount === 0) {
+        return { message: 'Artist not found or already deleted', status: 404 };
+      }
+
+      return {message: 'Artist was successfully deleted', status: 200}
+    }catch (e) {
+      console.error('Invalid server error' + e);
+    }
   }
 }
