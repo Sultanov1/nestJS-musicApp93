66c1e48faa90ef1bb238407a0ef51b8aadc18da4
Oybek Sultanov <oibekzzz09@gmail.com>
2024-10-26 20:24:05 +0600

    Содание модели пользователя и также добавления роли по тз.

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,64 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import {randomUUID} from 'crypto';
+import { compare, genSalt, hash } from 'bcrypt';
+import { Document } from 'mongoose';
+
+export interface UserMethods {
+  generateToken: () => void;
+  checkPassword: (password: string) => Promise<boolean>;
+}
+
+export type UserDocument = User & Document & UserMethods;
+
+const SALT_WORD_FACTORY = 10;
+
+@Schema()
+export class User {
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true })
+  token: string;
+
+  @Prop({
+    enum: ['user', 'admin'],
+    required: true,
+    default: 'user'
+  })
+  role: string;
+
+  @Prop()
+  displayName: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.methods.generateToken = function() {
+  this.token = randomUUID();
+}
+
+UserSchema.methods.checkPassword = function(password: string) {
+  return compare(password, this.password);
+}
+
+UserSchema.pre<UserDocument>('save',async function() {
+  if (!this.isModified('password')) {
+    return;
+  }
+
+  const salt = await genSalt(SALT_WORD_FACTORY);
+  this.password = await hash(this.password, salt);
+})
+
+UserSchema.set('toJSON', {
+  transform: (_doc, ret) => {
+    delete ret.password;
+    return ret;
+  },
+})
